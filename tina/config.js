648b9c9cd48720572f43dce516fe69bae48ff9c3
Ranju <141168492+RanjuLG@users.noreply.github.com>
2025-08-30 02:55:06 +0530

    initial

--- tina/config.js
+++ tina/config.js
@@ -1,33 +1,51 @@
 import { defineConfig } from "tinacms";
-import page from "./collections/page";
-import post from "./collections/post";
 
-export const config = defineConfig({
-  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
-  branch:
-    process.env.NEXT_PUBLIC_TINA_BRANCH || // custom branch env override
-    process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF || // Vercel branch env
-    process.env.HEAD, // Netlify branch env
-  token: process.env.TINA_TOKEN,
+const branch = process.env.GITHUB_BRANCH || "main";
+const clientId = process.env.TINA_CLIENT_ID;
+const token = process.env.GITHUB_TOKEN;
+
+export default defineConfig({
+  branch,
+  clientId,
+  token,
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
   media: {
-    // If you wanted cloudinary do this
-    // loadCustomStore: async () => {
-    //   const pack = await import("next-tinacms-cloudinary");
-    //   return pack.TinaCloudCloudinaryMediaStore;
-    // },
-    // this is the config for the tina cloud media store
     tina: {
+      mediaRoot: "",
       publicFolder: "public",
-      mediaRoot: "uploads",
     },
   },
-  build: {
-    publicFolder: "public", // The public asset folder for your framework
-    outputFolder: "admin", // within the public folder
-  },
   schema: {
-    collections: [page, post],
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "datetime",
+            name: "date",
+            label: "Date",
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
   },
 });
-
-export default config;
